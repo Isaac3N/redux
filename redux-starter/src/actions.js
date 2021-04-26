46eceb3bcdd5a066797776dfde80383c7c42aa04
Isaac3N <theisaac3n@gmail.com>
2021-04-26 22:00:34 +0100

    redux-starter

--- redux-starter/src/actions.js
+++ redux-starter/src/actions.js
@@ -1,1 +1,10 @@
-export  function bugAdded(description)
+import * as actions from './actionTypes'
+
+export  function bugAdded(description){
+    return {
+        type: actions.BUG_ADDED,
+        payload: {
+            description: 'Bug1'
+        }
+    }
+}
